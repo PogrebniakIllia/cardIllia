04d0e2aa3a004d9ebc6467992c84d0f8f2069ff3
croissant🥐 <illiapogrebniak@gmail.com>
2024-11-20 18:20:25 +0200

    change name, create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let git = document.querySelector('.github');
+let inst = document.querySelector('.inst');
+let tg = document.querySelector('.tg')
+
+git.addEventListener('click', function() {
+    location.assign('https://github.com/PogrebniakIllia')
+});
+
+inst.addEventListener('click', function() {
+    location.assign('https://www.instagram.com/')
+});
+
+tg.addEventListener('click', function() {
+    location.assign('https://web.telegram.org/k/')
+})
